0e88c3a1c24197726e29619fee7b2098caeafcf9
quadrat137 <alexgsh404@gmail.com>
2019-08-11 00:29:49 +0200

    gli is ready

--- gli.py
+++ gli.py
@@ -2,14 +2,45 @@
 import os
 import sys
 
-if __name__ == '__main__':
 
-    branches = list(map(lambda line: line.strip(), os.popen('git branch ').read().split("\n")))
+def main(params):
+    raw_branches = os.popen('git branch ').read().split("\n")
+    branches = list(filter(lambda line: line, raw_branches))
 
-    if len(sys.argv) > 1:
-        name_part = sys.argv[1]
+    if len(params) > 1:
+        name_part = params[1]
         if name_part:
             print(f"name_part: {name_part}")
-            branches = list(filter(lambda branch: name_part in branch, branches))
+            branches = list(filter(lambda x: name_part in x, branches))
 
     print(f"branches: {branches}")
+
+    if len(branches) > 9:
+        print("too much branches match your query")
+        for branch in branches[:9]:
+            print(branch)
+        print(f"and {len(branches) - 9} more")
+        return
+    elif len(branches) is 0:
+        print("no branches match your query")
+        return
+
+    elif len(branches) is 1:
+        print("one branch match")
+        result_branch = branches[0]
+    else:
+        print("select branch: ")
+        for index, branch in enumerate(branches):
+            print(f"{index}, {branch}")
+        read_index = input()
+        result_branch = branches[int(read_index)]
+
+    if '*' in result_branch:
+        result_branch = result_branch.replace('*', '').strip()
+
+    print(f"checking out branch: {result_branch}")
+    os.popen(f"git checkout {result_branch}")
+
+
+if __name__ == '__main__':
+    main(sys.argv)
