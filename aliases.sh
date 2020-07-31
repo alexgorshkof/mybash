897e3afac864be099f8d362b9a955b9061470267
quadrat137 <alexgsh404@gmail.com>
2020-07-31 22:12:38 +0200

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-#Directory aliases
-alias cdp='cd ~/stash.int.klarna.net/PER'
-alias cdg='cd ~/stash.int.klarna.net/GEMS'
-
 #GIT aliases
 alias gt='cd ~/GIT'
 alias gf='git fetch'
